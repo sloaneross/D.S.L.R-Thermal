b3182d02e25c349c959188660cb28862c688ff6b
Sloane Ross <sloaneross113@gmail.com>
2024-08-27 16:59:29 -0400

    print vs upload bifurcation

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ import * as  fs from 'fs';
 import * as http from 'http';
 
 // This finds and opens the printer, sets the encoding
-// const device = new USB();
-// const options = { encoding: "utf8" /* default */ }
-// let printer = new Printer(device, options);
+const device = new USB();
+const options = { encoding: "utf8" /* default */ }
+let printer = new Printer(device, options);
 
 function uploadImage(fullPath) {
     var form = new FormData();
     //form.append('my_field', 'file');
     form.append('file', fs.createReadStream(fullPath));
 
-    form.submit("http://lesbianreflex.com/upload", function(err, res){
+    form.submit("https://www.lesbianreflex.com/upload", function(err, res){
         console.log(res.statusCode);
         console.log(res.statusMessage);
         res.resume();
@@ -28,23 +28,23 @@ function uploadImage(fullPath) {
 
 // @param fullImagePath is the full (not relative) path to the *image file* to print
 // @out prints the image along with the file name without path or extention
-// function printImage(fullImagePath) {
-//     var filename = parse(fullImagePath).name;
-//     device.open(async function(err){
-//         if(err){
-//           //TODO handle error (try?)
-//           return
-//         }        
+function printImage(fullImagePath) {
+    var filename = parse(fullImagePath).name;
+    device.open(async function(err){
+        if(err){
+          //TODO handle error (try?)
+          return
+        }        
       
-//         const filePath = join(fullImagePath);
-//         const image = await Image.load(filePath);
-//         await printer.image(image, "d24")
-//         printer
-//             .text(`${filename}`)
-//             .feed(2)
-//         printer.close()
-//       });
-// }
+        const filePath = join(fullImagePath);
+        const image = await Image.load(filePath);
+        await printer.image(image, "d24")
+        printer
+            .text(`${filename}`)
+            .feed(2)
+        printer.close()
+      });
+}
 
 // wathces the selected folder, ignores dot files
 const watcher = chokidar.watch("/Users/sloaneross/Desktop/PrintingFolder", {
@@ -54,6 +54,20 @@ const watcher = chokidar.watch("/Users/sloaneross/Desktop/PrintingFolder", {
 
 // When files are added to the watched folder, logs the path and prints.
 watcher
+    .on('add', path => {
+        console.log(`File ${path} has been added`)
+        printImage(path)
+        //uploadImage(path)
+    }
+    )
+
+const watcher2 = chokidar.watch("/Users/sloaneross/Desktop/UploadingFolder", {
+    ignored: /(^|[\/\\])\../,
+    persistant: true
+})
+
+// When files are added to the watched folder, logs the path and prints.
+watcher2
     .on('add', path => {
         console.log(`File ${path} has been added`)
         //printImage(path)
